7a922251f5b316dd4885083f6ebc818541d43770
changshu.li <lcs.005@163.com>
2020-10-13 10:03:57 +0800

    myPow Good

--- src/test/java/lee/t/code/PowXN.java
+++ src/test/java/lee/t/code/PowXN.java
@@ -3,68 +3,63 @@ package lee.t.code;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ * <br />
+ * https://leetcode-cn.com/problems/powx-n/
+ */
 public class PowXN {
+
     public double myPow(double x, int n) {
         if (x == 0) {
             return 0;
         }
-        long nb = n;
-        if (nb < 0) {
-            x = 1 / x;
-            nb = 0 - nb;
-        }
-        return pow(x, nb);
-    }
-
-    private double pow(double x, long n) {
-        if (n > 2L * Integer.MAX_VALUE) {
-            throw new IllegalArgumentException("Max 0x7fffffff");
-        }
         if (n == 0) {
             return 1;
         }
-        double next = x;
-        long i = 1;
-        for (; ; ) {
-            long t = i * 2;
-            if (t > n) {
-                break;
+        return pow(x, n);
+    }
+
+    private double pow(double x, int n) {
+        double next = x, res = 1;
+        for (int i = n; i != 0; i /= 2) {
+            if (i % 2 != 0) {
+                res *= next;
             }
-            i = t;
             next *= next;
         }
-        return next * pow(x, n - i);
+        return n < 0 ? 1 / res : res;
     }
 
     @Test
     public void test() {
         {
-            double x = 0.00001; int n = 2147483647;
+            double x = 65;
+            int n = 7;
             double v = this.myPow(x, n);
             Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
         }
         {
-            double x = 1.0; int n = -2147483648;
+            double x = 0.00001;
+            int n = 2147483647;
             double v = this.myPow(x, n);
             Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
         }
         {
-            double x = 0; int n = -5;
+            double x = 1.0;
+            int n = -2147483648;
             double v = this.myPow(x, n);
             Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
         }
         {
-            double x = 65; int n = 0;
+            double x = 65;
+            int n = 0;
             double v = this.myPow(x, n);
             Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
         }
         {
-            double x = 65; int n = 7;
-            double v = this.myPow(x, n);
-            Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
-        }
-        {
-            double x = 65; int n = -5;
+            double x = 65;
+            int n = -5;
             double v = this.myPow(x, n);
             Assert.assertEquals(Math.pow(x, n), v, 0.00000000000000001);
         }
