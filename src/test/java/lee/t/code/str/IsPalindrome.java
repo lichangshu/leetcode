28eda4475988c73613e258ae2ddbff6f02f6705d
changshu.li <lcs.005@163.com>
2021-11-26 18:55:40 +0800

    StrStr

--- src/test/java/lee/t/code/str/IsPalindrome.java
+++ src/test/java/lee/t/code/str/IsPalindrome.java
@@ -6,6 +6,23 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 验证回文串
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ *
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 解释："amanaplanacanalpanama" 是回文串
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xne8id/
+ */
 public class IsPalindrome {
     enum Solution {
         loop {
--- src/test/java/lee/t/code/str/StrStr.java
+++ src/test/java/lee/t/code/str/StrStr.java
@@ -3,32 +3,71 @@ package lee.t.code.str;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 实现 strStr()
+ * 实现strStr()函数。
+ * <p>
+ * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回 -1 。
+ * <p>
+ * <p>
+ * <p>
+ * 说明：
+ * <p>
+ * 当needle是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当needle是空字符串时我们应当返回 0 。这与 C 语言的strstr()以及 Java 的indexOf()定义相符。
+ * <p>
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnr003/
+ */
 public class StrStr {
     enum Solution {
+        loop2loop {// Timeout
+
+            @Override
+            public int strStr(String haystack, String needle) {
+                if (needle.length() == 0) return 0;
+                ; //;;;;
+                int olen = haystack.length();
+                int nlen = needle.length();
+                for (int i = 0; i < olen; i++) {
+                    int j = 0;
+                    for (; j < nlen && i + j < olen; j++) {
+                        if (haystack.charAt(i + j) != needle.charAt(j)) {
+                            break;
+                        }
+                    }
+                    if (j == nlen) return i;
+                }
+                return -1;
+            }
+        },
         loop {// Timeout
 
             @Override
             public int strStr(String haystack, String needle) {
                 if (needle.length() == 0) return 0;
                 ; //;;;;
-                int val = haystack.length();
-                int len = needle.length();
-                for (int i = 0; i < val; i++) {
-                    for (int j = 0; j < len && i + j < val; j++) {
-                        char ch = haystack.charAt(i + j);
-                        char c2 = needle.charAt(j);
-                        if (c2 != ch) {
-                            break;
-                        }
-                        if (j + 1 == len) return i;
+                int olen = haystack.length();
+                int nlen = needle.length();
+                int i = 0, j = 0;
+                for (; i < olen && j < nlen; ) {
+                    if (haystack.charAt(i) == needle.charAt(j)) {
+                        j++;
+                    } else {
+                        i = i - j; // back
+                        j = 0;
                     }
+                    i++;
                 }
+                if (j == nlen) return i - j;
                 return -1;
             }
         },
         ;
 
         public abstract int strStr(String haystack, String needle);
+
     }
 
     @Test
@@ -37,6 +76,9 @@ public class StrStr {
             Assert.assertEquals(0, value.strStr("aaaaaaa", ""));
             Assert.assertEquals(2, value.strStr("aaTop", "Top"));
             Assert.assertEquals(-1, value.strStr("aaa", "aaaaa"));
+            Assert.assertEquals(4, value.strStr("mississippi", "issip"));
+            Assert.assertEquals(-1, value.strStr("baaa", "abbb"));
+            Assert.assertEquals(4, value.strStr("aabaaabaaac", "aabaaac"));
         }
     }
 }
